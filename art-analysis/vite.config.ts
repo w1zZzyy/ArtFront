b59e53b0717455e6f600777c11dd47274ac5f47f
w1zZzyy <dkilikki22@gmail.com>
2025-12-14 23:21:40 +0300

    lab6 https

--- art-analysis/vite.config.ts
+++ art-analysis/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import fs from 'fs'
+import path from 'path'
 
 export default defineConfig({
   base: '/ArtFront/', 
@@ -42,6 +44,10 @@ export default defineConfig({
     })
   ],
   server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
+    },
     port: 5173,
     proxy: {
       '/api': {
