b3af40aaaac02738138053269fdf56f567fabd88
w1zZzyy <dkilikki22@gmail.com>
2025-12-14 18:07:40 +0300

    lab6 dev-dist

--- art-analysis/dev-dist/sw.js
+++ art-analysis/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "c42d79354dbf60ad2f5d4cc715ce3c11"
   }, {
     "url": "index.html",
-    "revision": "0.8i1687dti9s"
+    "revision": "0.b972b43n76g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
