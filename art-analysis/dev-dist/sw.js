b31c3150524d07510954289d41e4066938b862a5
w1zZzyy <dkilikki22@gmail.com>
2025-12-16 23:51:52 +0300

    lab5 карусель

--- art-analysis/dev-dist/sw.js
+++ art-analysis/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "c42d79354dbf60ad2f5d4cc715ce3c11"
   }, {
     "url": "index.html",
-    "revision": "0.i26ik8nbmqo"
+    "revision": "0.5kujeik749o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- art-analysis/src/pages/StartPage.tsx
+++ art-analysis/src/pages/StartPage.tsx
@@ -1,8 +1,18 @@
 // src/pages/HomePage.tsx
 import { AppNavbar } from '../components/Navbar';
+import { Carousel } from 'react-bootstrap';
 import './styles/StartPage.css';
 
 export const HomePage = () => {
+    const artworks = [
+        { src: '/ArtFront/images/abstract_1.jpg', title: 'Композиция 1', description: 'Анализ линий и форм' },
+        { src: '/ArtFront/images/abstract_2.jpg', title: 'Композиция 2', description: 'Цветовой баланс' },
+        { src: '/ArtFront/images/abstract_3.jpg', title: 'Композиция 3', description: 'Перспектива и глубина' },
+        { src: '/ArtFront/images/abstract_4.jpg', title: 'Композиция 4', description: 'Ритм и движение' },
+        { src: '/ArtFront/images/abstract_5.jpg', title: 'Композиция 5', description: 'Контраст и гармония' },
+        { src: '/ArtFront/images/abstract_6.jpg', title: 'Композиция 6', description: 'Фокус внимания' },
+    ];
+
     return (
         <div className="homepage-wrapper">
             <AppNavbar />
@@ -16,14 +26,24 @@ export const HomePage = () => {
                         <p className="home-page-lead">
                             Инструменты и эксперты для изучения замысла художника через композицию
                         </p>
+                    </div>
 
-                        {/* Действия можно подключить позже */}
-                        {/*
-                        <div className="home-page-actions">
-                            <a href="/experts" className="home-page-btn primary">Выбрать эксперта</a>
-                            <a href="/tasks" className="home-page-btn secondary">Мои задачи</a>
-                        </div>
-                        */}
+                    <div className="carousel-content">
+                        <Carousel interval={3000} pause="hover">
+                            {artworks.map((artwork, index) => (
+                                <Carousel.Item key={index}>
+                                    <img
+                                        className="d-block carousel-image"
+                                        src={artwork.src}
+                                        alt={artwork.title}
+                                    />
+                                    <Carousel.Caption className="carousel-caption-custom">
+                                        <h3>{artwork.title}</h3>
+                                        <p>{artwork.description}</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                            ))}
+                        </Carousel>
                     </div>
                 </div>
             </div>
